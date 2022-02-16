e0e62add8f9856a643b86d538219b5b10f58cee8
RobbingDaHood <he@nykredit.dk>
2022-02-16 17:48:59 +0100

    added struct Config.struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,44 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
 
-    println!("query: {}", query);
-    println!("filename: {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problems parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(filename)
+    println!("Searching for {}", config.query());
+    println!("In file {}", config.filename());
+
+    let contents = fs::read_to_string(config.filename())
         .expect("File read error");
 
     println!("Content of file: \n{}", contents)
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename})
+    }
+
+    pub fn query(&self) -> &str {
+        &self.query
+    }
+    pub fn filename(&self) -> &str {
+        &self.filename
+    }
 }
