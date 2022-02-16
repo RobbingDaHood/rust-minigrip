8d08765182334e3fd7dd9a97fd6c1e20790fc5bd
RobbingDaHood <he@nykredit.dk>
2022-02-16 17:27:12 +0100

    read file from disk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("query: {}", query);
+    println!("filename: {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("File read error");
+
+    println!("Content of file: \n{}", contents)
 }
