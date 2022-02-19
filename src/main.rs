7e2a822d1810bbaa63310f42b8981c6724b068e4
RobbingDaHood <he@nykredit.dk>
2022-02-19 12:19:49 +0100

    Better error handling and refactor of code into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::{env, fs, process};
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = minigrip::Config::new(&args).unwrap_or_else(|err| {
         println!("Problems parsing arguments: {}", err);
         process::exit(1);
     });
@@ -12,33 +12,8 @@ fn main() {
     println!("Searching for {}", config.query());
     println!("In file {}", config.filename());
 
-    let contents = fs::read_to_string(config.filename())
-        .expect("File read error");
-
-    println!("Content of file: \n{}", contents)
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename})
-    }
-
-    pub fn query(&self) -> &str {
-        &self.query
-    }
-    pub fn filename(&self) -> &str {
-        &self.filename
+    if let Err(e) = minigrip::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
